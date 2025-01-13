1ddba7098f4f61efe0d79ed643fbbb0b09b5624f
Dimodic <167333404+Dimodic@users.noreply.github.com>
2025-01-13 11:34:14 +0300

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+from .config import CONTEST_IDS, HEADER_ROWS_COUNT
+from .google_sheets import get_gspread_client, get_worksheet
+from .utils import build_login2row, index_to_column_letter, parse_homework_headers
+from .yandex_contest import get_contest_problems, get_contest_participants, get_all_submissions
+
+__all__ = [
+    "CONTEST_IDS",
+    "HEADER_ROWS_COUNT",
+    "get_gspread_client",
+    "get_worksheet",
+    "build_login2row",
+    "index_to_column_letter",
+    "parse_homework_headers",
+    "get_contest_problems",
+    "get_contest_participants",
+    "get_all_submissions",
+]
