2182ac936bc09a44fdf1867be97cc6c540f21896
Dimodic <167333404+Dimodic@users.noreply.github.com>
2025-01-13 13:01:33 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,19 @@
 from .config import CONTEST_IDS, HEADER_ROWS_COUNT
-from .google_sheets import get_gspread_client, get_worksheet
-from .utils import build_login2row, index_to_column_letter, parse_homework_headers
-from .yandex_contest import get_contest_problems, get_contest_participants, get_all_submissions
+from .yandex_contest import (
+    get_contest_problems,
+    get_contest_participants,
+    get_all_submissions
+)
+from .google_sheets import (
+    get_gspread_client,
+    get_worksheet
+)
+from .utils import (
+    build_login2row,
+    index_to_column_letter,
+    parse_homework_headers
+)
+
 
 __all__ = [
     "CONTEST_IDS",
