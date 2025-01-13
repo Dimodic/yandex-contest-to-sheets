b06dfed0dc4fbc4a429c96444b59072e04105ae6
Dimodic <167333404+Dimodic@users.noreply.github.com>
2025-01-13 13:43:44 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,7 +8,7 @@ CONTEST_IDS = os.getenv("CONTEST_IDS", "12345").split(",")
 YANDEX_OAUTH_TOKEN = os.getenv("YANDEX_OAUTH_TOKEN", "<token>")
 BASE_URL = os.getenv("BASE_URL", "https://api.contest.yandex.net/api/public/v2")
 
-SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE", "service_account.json")
+SERVICE_ACCOUNT_FILE = os.path.join(os.path.dirname(__file__), "..", "service_account.json")
 SPREADSHEET_ID = os.getenv("SPREADSHEET_ID", "<spreadsheet-id>")
 
 HEADER_ROWS_COUNT = int(os.getenv("HEADER_ROWS_COUNT", 2))
