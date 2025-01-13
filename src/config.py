23aef78621b299975cdae03db9e6cea44c785b47
Dimodic <167333404+Dimodic@users.noreply.github.com>
2025-01-13 12:38:54 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,14 @@
-CONTEST_IDS = [
-    id
-]
+import os
+from dotenv import load_dotenv
 
-YANDEX_OAUTH_TOKEN = "<token>"
-BASE_URL = "https://api.contest.yandex.net/api/public/v2"
+load_dotenv()
 
-SERVICE_ACCOUNT_FILE = "service_account.json"
-SPREADSHEET_ID = "<id>"
+CONTEST_IDS = os.getenv("CONTEST_IDS", "12345").split(",")
 
-HEADER_ROWS_COUNT = 2
+YANDEX_OAUTH_TOKEN = os.getenv("YANDEX_OAUTH_TOKEN", "<token>")
+BASE_URL = os.getenv("BASE_URL", "https://api.contest.yandex.net/api/public/v2")
+
+SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE", "service_account.json")
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID", "<spreadsheet-id>")
+
+HEADER_ROWS_COUNT = int(os.getenv("HEADER_ROWS_COUNT", 2))
