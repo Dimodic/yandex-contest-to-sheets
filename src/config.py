62446eea6fc241618978a7acb9fc5579c3e32922
Dimodic <167333404+Dimodic@users.noreply.github.com>
2025-01-19 18:21:56 +0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -6,7 +6,10 @@ load_dotenv()
 # === Yandex Contest API конфигурация ===
 YANDEX_OAUTH_TOKEN = os.getenv("YANDEX_OAUTH_TOKEN")
 BASE_URL = os.getenv("BASE_URL")
-CONTEST_IDS = os.getenv("CONTEST_IDS").split(",")
+CONTEST_IDS = {
+    int(pair.split(":")[0]): int(pair.split(":")[1])
+    for pair in os.getenv("CONTEST_IDS").split(",")
+}
 
 # === Google Sheets API конфигурация ===
 SERVICE_ACCOUNT_FILE = os.path.join(os.path.dirname(__file__), "..", "service_account.json")
